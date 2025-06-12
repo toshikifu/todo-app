4f76f0ede1829647ab0b77d93196e50bd8ce97f5
Toshiki <toshiki.fujii@outlook.com>
2025-06-12 17:48:30 +0900

    setup tanstack query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "../src/components/ui/provider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>
 );

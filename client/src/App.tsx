a0c488f1f75745c731e2fb2b466f42704639cd73
Toshiki <toshiki.fujii@outlook.com>
2025-06-12 18:08:05 +0900

    update UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
+import TodoList from "../src/components/TodoList";
+import TodoForm from "../src/components/TodoForm";
 import Navbar from "../src/components/Navbar";
 import { Container, Stack } from "@chakra-ui/react";
+
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api";
 
 function App() {
   return (
     <Stack h="100vh">
       <Navbar />
       <Container>
-        {/* <TodoForm />
-        <TodoList /> */}
+        <TodoForm />
+        <TodoList />
       </Container>
     </Stack>
   );
